f909646ce45785aab26b56f28510cc24d3d4d264
Jane Sinah <janesinah2006@outlook.com>
2025-11-28 13:14:22 +0000

    added the nav bar and the add to basket button now has some functionality

--- productPage.php
+++ productPage.php
@@ -1,15 +1,54 @@
+<?php 
+    include('db.php');
+    session_start();
+
+    $_SESSION["userID"] = "1"; //some hard coding ill use for now until the user account functionality has been made
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submitted'])) { 
+        $user_id = $_SESSION['userID'];
+        $productID = $_POST['productID'];
+        $productSize = $_POST['sizes'];
+        $productQuantity = $_POST['quantity'];
+        
+         $stmt = $db->prepare("INSERT INTO baskets (userID, productID, quantity, size) VALUES (?, ?, ?, ?)");
+         $stmt->execute([$user_id, $productID, $productQuantity, $productSize]);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo Product</title>
     <link href="styles.css" rel="stylesheet">
+    <link href="navbarstyling.css" rel="stylesheet">
     <script src="scripts.js" defer></script>
 </head>
 
 <body>
+  <div class="navbar">
+  <div class="nav-left">
+    <div class="sidebar-icon">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </div>
+    <div class="brand-logo">
+      <img src="Urban42logo.png" alt="Urban 42 Logo">
+      <span>Urban 42</span>
+    </div>
+  </div>
+  <div class="nav-right">
+    <img src="ukflag.png" alt="UK Flag" class="flag-icon">
+    <span>GBP £</span>
+    <a href="#">Help</a>
+    <a href="#">Log in</a>
+    <a><form class="search-form">
+      <input type="text" placeholder="Search..." name="search">
+      <button type="submit">🔍</button>
+    </form></a>
+    <a href="#">Cart</a>
+    </div>
+  </div>
     <div id="product-details">
         <div class="carousel" data-carousel aria-label="Product Photos">
             <button class="carousel-button prev" data-carousel-button="prev">&#60;</button>
@@ -37,10 +76,11 @@
                 <p>In Stock</p>
             </div>
             <div id="short-description">
-                <p>A sleek yet casual style trainer with a futurustic black design. Paired with comfortable cushioned
+                <p>A sleek yet casual style trainer with a futuristic black design. Paired with comfortable cushioned
                     soles, these trainers are perfect for everyday wear.</p>
             </div>
-            <form>
+            <form method="POST">
+                <input type="hidden" name="submitted" value="1">
                 <div id="inputSize">
                     <label>Size: </label><br>
                     <select name="sizes" class="sizes" required>
